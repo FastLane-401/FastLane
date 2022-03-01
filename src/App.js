1ea8519a2892d0934fbd717135b67e74b2f0e292
Zachery Creech <zcreech@vols.utk.edu>
2022-02-28 20:15:29 -0500

    Added voice recognition and writing voice input to file on button press.

--- src/App.js
+++ src/App.js
@@ -6,8 +6,284 @@
  * @flow strict-local
  */
 
-import React from 'react'
+import React, {Component} from 'react'
+
 import {
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  TouchableHighlight,
+} from 'react-native';
+
+import Voice, {
+  SpeechRecognizedEvent,
+  SpeechResultsEvent,
+  SpeechErrorEvent,
+} from '@react-native-voice/voice';
+
+type Props = {};
+type State = {
+  recognized: string;
+  pitch: string;
+  error: string;
+  end: string;
+  started: string;
+  results: string[];
+  partialResults: string[];
+};
+
+class VoiceTest extends Component<Props, State> {
+  state = {
+    recognized: '',
+    pitch: '',
+    error: '',
+    end: '',
+    started: '',
+    results: [],
+    partialResults: [],
+    read: '',
+  };
+
+  constructor(props: Props) {
+    super(props);
+    Voice.onSpeechStart = this.onSpeechStart;
+    Voice.onSpeechRecognized = this.onSpeechRecognized;
+    Voice.onSpeechEnd = this.onSpeechEnd;
+    Voice.onSpeechError = this.onSpeechError;
+    Voice.onSpeechResults = this.onSpeechResults;
+    Voice.onSpeechPartialResults = this.onSpeechPartialResults;
+    Voice.onSpeechVolumeChanged = this.onSpeechVolumeChanged;
+  }
+
+  componentWillUnmount() {
+    Voice.destroy().then(Voice.removeAllListeners);
+  }
+
+  onSpeechStart = (e: any) => {
+    console.log('onSpeechStart: ', e);
+    this.setState({
+      started: '!',
+    });
+  };
+
+  onSpeechRecognized = (e: SpeechRecognizedEvent) => {
+    console.log('onSpeechRecognized: ', e);
+    this.setState({
+      recognized: '√',
+    });
+  };
+
+  onSpeechEnd = (e: any) => {
+    console.log('onSpeechEnd: ', e);
+    this.setState({
+      end: '√',
+    });
+  };
+
+  onSpeechError = (e: SpeechErrorEvent) => {
+    console.log('onSpeechError: ', e);
+    this.setState({
+      error: JSON.stringify(e.error),
+    });
+  };
+
+  onSpeechResults = (e: SpeechResultsEvent) => {
+    console.log('onSpeechResults: ', e);
+    this.setState({
+      results: e.value,
+    });
+    var RNFS = require('react-native-fs');
+    var path = RNFS.DocumentDirectoryPath + '/voicelog.txt';
+
+    /* if you want text to persist in the file between button presses, use
+     * appendFile() instead of writeFile(). You should also probably modify
+     * the second parameter to ' ' + e.value[0] so text strings don't run
+     * together between button presses. */
+    RNFS.writeFile(path, e.value[0], 'utf8')
+      .then((success) => {
+        console.log('FILE WRITTEN');
+      })
+      .catch((err) => {
+        console.log(err.message);
+      });
+  };
+
+
+  onSpeechPartialResults = (e: SpeechResultsEvent) => {
+    console.log('onSpeechPartialResults: ', e);
+    this.setState({
+      partialResults: e.value,
+    });
+  };
+
+  onSpeechVolumeChanged = (e: any) => {
+    console.log('onSpeechVolumeChanged: ', e);
+    this.setState({
+      pitch: e.value,
+    });
+  };
+
+  _startRecognizing = async () => {
+    this.setState({
+      recognized: '',
+      pitch: '',
+      error: '',
+      started: '',
+      results: [],
+      partialResults: [],
+      end: '',
+    });
+
+    try {
+      await Voice.start('en-US');
+    } catch (e) {
+      console.error(e);
+    }
+  };
+
+  _stopRecognizing = async () => {
+    try {
+      await Voice.stop();
+    } catch (e) {
+      console.error(e);
+    }
+  };
+
+  _cancelRecognizing = async () => {
+    try {
+      await Voice.cancel();
+    } catch (e) {
+      console.error(e);
+    }
+  };
+
+  _destroyRecognizer = async () => {
+    try {
+      await Voice.destroy();
+    } catch (e) {
+      console.error(e);
+    }
+    this.setState({
+      recognized: '',
+      pitch: '',
+      error: '',
+      started: '',
+      results: [],
+      partialResults: [],
+      end: '',
+    });
+  };
+
+  _readVoiceLog = async () => {
+    try {
+      var RNFS = require('react-native-fs');
+      var path = RNFS.DocumentDirectoryPath + '/voicelog.txt';
+
+      RNFS.readFile(path, 'utf8')
+        .then((data) => {
+          console.log('FILE READ');
+          this.setState({
+            read: data,
+          });
+        })
+        .catch((err) => {
+          console.log(err.message);
+        });
+    } catch (e) {
+      console.error(e);
+    }
+  };
+
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.welcome}>Welcome to FastLane!</Text>
+        <Text style={styles.instructions}>
+          Press the button and start speaking.
+        </Text>
+        <Text style={styles.stat}>{`Started: ${this.state.started}`}</Text>
+        <Text style={styles.stat}>{`Recognized: ${
+          this.state.recognized
+        }`}</Text>
+        <Text style={styles.stat}>{`Pitch: ${this.state.pitch}`}</Text>
+        <Text style={styles.stat}>{`Error: ${this.state.error}`}</Text>
+        <Text style={styles.stat}>Results</Text>
+        {this.state.results.map((result, index) => {
+          return (
+            <Text key={`result-${index}`} style={styles.stat}>
+              {result}
+            </Text>
+                  );
+        })}
+        <Text style={styles.stat}>Partial Results</Text>
+        {this.state.partialResults.map((result, index) => {
+          return (
+            <Text key={`partial-result-${index}`} style={styles.stat}>
+              {result}
+            </Text>
+          );
+        })}
+        <Text style={styles.stat}>{`End: ${this.state.end}`}</Text>
+        <Text style={styles.stat}>{`This was written to the file: ${this.state.read}`}</Text>
+        <TouchableHighlight onPress={this._startRecognizing}>
+          <Image style={styles.button} source={require('./button.png')} />
+        </TouchableHighlight>
+        <TouchableHighlight onPress={this._stopRecognizing}>
+          <Text style={styles.action}>Stop Recognizing</Text>
+        </TouchableHighlight>
+        <TouchableHighlight onPress={this._cancelRecognizing}>
+          <Text style={styles.action}>Cancel</Text>
+        </TouchableHighlight>
+        <TouchableHighlight onPress={this._destroyRecognizer}>
+          <Text style={styles.action}>Destroy</Text>
+        </TouchableHighlight>
+        <TouchableHighlight onPress={this._readVoiceLog}>
+          <Text style={styles.action}>Read</Text>
+        </TouchableHighlight>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  button: {
+    width: 50,
+    height: 50,
+  },
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  action: {
+    textAlign: 'center',
+    color: '#0000FF',
+    marginVertical: 5,
+    fontWeight: 'bold',
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+  stat: {
+    textAlign: 'center',
+    color: '#B0171F',
+    marginBottom: 1,
+  },
+});
+
+export default VoiceTest;
+
+/*import {
   SafeAreaView,
   ScrollView,
   StatusBar,
@@ -80,4 +356,4 @@ const styles = StyleSheet.create({
   }
 })
 
-export default App
+export default App*/
